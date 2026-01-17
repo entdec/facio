2d3ad883544f7cc955e67a508edda77c5b7ee927
Tom de Grunt <tom@degrunt.nl>
2026-01-17 17:40:24 +0000

    A little cleaner

--- lib/facio/concerns/callbacks.rb
+++ lib/facio/concerns/callbacks.rb
@@ -2,7 +2,12 @@ module Callbacks
   extend ActiveSupport::Concern
 
   included do
-    around_perform do |job, block|
+    around_perform :_facio_set_context_and_result_for_perform
+    around_enqueue :_facio_set_context_for_enqueue
+
+    private
+
+    def _facio_set_context_and_result_for_perform
       @context = self.class.context_class.new(arguments.first)
       @result = self.class.result_class&.new
       @performed = false
@@ -11,13 +16,13 @@ module Callbacks
         result = nil
         if transactional && defined?(ActiveRecord::Base)
           ActiveRecord::Base.transaction(requires_new: true) do
-            result = block.call
+            result = yield
             # This will only rollback the changes of the service, SILENTLY, however the context will be failed? already.
             # This is the most close to expected behaviour this can get.
             raise ActiveRecord::Rollback if context.failed?
           end
         else
-          result = block.call
+          result = yield
         end
 
         @performed = true
@@ -26,12 +31,12 @@ module Callbacks
       end
     end
 
-    around_enqueue do |job, block|
+    def _facio_set_context_for_enqueue
       @performed = false
       @context = self.class.context_class.new(arguments.first)
       @result = self.class.result_class&.new
 
-      block.call if @context.valid?
+      yield if @context.valid?
     end
   end
 end
